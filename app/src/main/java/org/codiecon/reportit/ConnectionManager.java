f7f5043d4f507e8f0d6ae245fd88314cfe21f41a
anand <anand.kumar@gdn-commerce.com>
2019-03-09 19:24:15 +0530

    changes

--- app/src/main/java/org/codiecon/reportit/ConnectionManager.java
+++ app/src/main/java/org/codiecon/reportit/ConnectionManager.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ConnectionManager extends Application {
 
-    private static final String BASE_URI = "http://10.177.68.119:8080/backend/";
+    private static final String BASE_URI = "http://8d67081e.ngrok.io/backend";
 
     private static ConnectionManager manager;
 
